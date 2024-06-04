6a293b207ec1068ffd1b64ca725cebb9ea967a32
Никита Фомин <defdXs@yandex.ru>
2024-06-04 21:50:25 +0300

    ML test api kinda work

---
+++ Backend/Backend.Web/Controllers/MachineLearningController.cs
@@ -0,0 +1,25 @@
+﻿using Backend.Processor;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Web.Controllers;
+
+/// <summary>
+/// SDG - Sustainable Development Goal
+/// </summary>
+
+[Route("api/ml")]
+[ApiController]
+public class MachineLearningController : ControllerBase
+{
+    public MachineLearningController() { }
+
+    [HttpGet]
+    [Route("{sdgid}&{tableid}")]
+    public async Task<IActionResult> PredictTable([FromRoute] int sdgid, [FromRoute] int tableid)
+    {
+        var result = IOProcess.Run("python ../Backend.ML/Testing/test.py Hello, web!").Output;
+
+        return Ok(result);
+    }
+}
+
