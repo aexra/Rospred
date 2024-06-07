d4290cf2a4df1ad6e85414e9441afd883b9dfa51
Никита Фомин <defdXs@yandex.ru>
2024-06-07 22:15:42 +0300

    Kinda refactoring

---
+++ Backend/Backend.Web/Controllers/SDGUtilityController.cs
@@ -0,0 +1,38 @@
+﻿using Backend.Web.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Web.Controllers;
+
+[Route("api/utils")]
+[ApiController]
+public class SDGUtilityController : ControllerBase
+{
+    private readonly SDGDBContext _context;
+
+    public SDGUtilityController(SDGDBContext context)
+    {
+        _context = context;
+    }
+
+
+    /// UTILITY
+
+
+    [HttpGet("tables/{id}")]
+    public async Task<ActionResult<IEnumerable<SDGTable>>> GetSDGTables(int id)
+    {
+        var sdg = await _context.SDGs.FindAsync(id);
+
+        if (sdg == null)
+        {
+            return NotFound();
+        }
+
+        var tablesIds = sdg.TableIds.Split(",").ToHashSet().Select(int.Parse);
+
+        var tables = _context.SDGTables.Where(t => tablesIds.Contains(t.Id));
+
+        return Ok(tables);
+    }
+}
