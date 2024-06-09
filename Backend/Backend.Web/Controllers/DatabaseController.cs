799a81080f5a46c78d68ab51469b2a601613af8e
Никита Фомин <defdXs@yandex.ru>
2024-06-09 15:17:44 +0300

    Added useful table insertions api requests

--- Backend/Backend.Web/Controllers/DatabaseController.cs
+++ Backend/Backend.Web/Controllers/DatabaseController.cs
@@ -38,7 +38,7 @@ public class DatabaseController : ControllerBase
             return NotFound($"Table {id} not found in Database");
         }
 
-        var dto = new TableDTO() { Id=table.Id, Name=table.Name };
+        var dto = new TableDTO() { Id=table.Id, Name=table.Name, SDG = table.SDG };
 
         if (!string.IsNullOrEmpty(table.ValuesIds))
         {
@@ -57,7 +57,7 @@ public class DatabaseController : ControllerBase
     }
 
     [HttpPost("table")]
-    public async Task<IActionResult> CreateTable([FromBody] TableDTO dto)
+    public async Task<IActionResult> CreateTable(TableDTO dto)
     {
         // Найдем указанный ЦУР
         var sdg = await _context.SDGs.FindAsync(dto.SDG);
@@ -88,7 +88,7 @@ public class DatabaseController : ControllerBase
         SDGTable resultEntity;
         try
         {
-            var t = new SDGTable() { Name = dto.Name, ValuesIds = string.Join(",", valuesIds) };
+            var t = new SDGTable() { Name = dto.Name, ValuesIds = string.Join(",", valuesIds), SDG = dto.SDG };
             var result = await _context.SDGTables.AddAsync(t);
             await _context.SaveChangesAsync();
             resultEntity = result.Entity;
@@ -102,7 +102,6 @@ public class DatabaseController : ControllerBase
         if (string.IsNullOrEmpty(sdg.TableIds))
         {
             sdg.TableIds = resultEntity.Id.ToString();
-            await _context.SaveChangesAsync();
         }
         else
         {
@@ -112,6 +111,131 @@ public class DatabaseController : ControllerBase
             await _context.SaveChangesAsync();
         }
 
+        await _context.SaveChangesAsync();
+        return Ok(resultEntity);
+    }
+
+    [HttpDelete("table/{id}")]
+    public async Task<IActionResult> DeleteTable(int id)
+    {
+        // Получим существующую таблицу по этому индексу
+        var table = await _context.SDGTables.FindAsync(id);
+        if (table == null) return NotFound($"Not found table {id} to replace");
+
+        // Сохраним номер ЦУР и имя таблицы
+        var sdgid = table.SDG;
+        var name = table.Name;
+
+        // Удалим все значения связанные со старой таблицей
+        if (!string.IsNullOrEmpty(table.ValuesIds))
+        {
+            var ids = table.ValuesIds.Split(",").Select(int.Parse).ToHashSet();
+            foreach (var vid in ids)
+            {
+                var value = await _context.SDGValues.FindAsync(vid);
+                if (value == null) continue;
+                _context.SDGValues.Remove(value);
+            }
+        }
+
+        // Удалим указатель на эту таблицу в указанном ЦУРе
+        var sdg = await _context.SDGs.FindAsync(sdgid);
+
+        if (sdg == null)
+        {
+            return NotFound($"SDG {sdgid} not found in Database");
+        }
+
+        var splitted = sdg.TableIds.Split(",").Select(int.Parse).ToHashSet();
+        splitted.Remove(id);
+        sdg.TableIds = string.Join(",", splitted);
+
+        // Удалим старую таблицу
+        _context.SDGTables.Remove(table);
+
+        await _context.SaveChangesAsync();
+
+        return Ok($"Removed table {id} ({name}) successfully");
+    }
+
+    [HttpPut("table")]
+    public async Task<IActionResult> PutTable(TableDTO dto)
+    {
+        // Получим существующую таблицу по этому индексу
+        var table = await _context.SDGTables.FindAsync(dto.Id);
+        if (table == null) return NotFound($"Not found table {dto.Id} to replace");
+
+        // Удалим все значения связанные со старой таблицей
+        if (!string.IsNullOrEmpty(table.ValuesIds))
+        {
+            var ids = table.ValuesIds.Split(",").Select(int.Parse).ToHashSet();
+            foreach (var id in ids)
+            {
+                var value = await _context.SDGValues.FindAsync(id);
+                if (value == null) continue;
+                _context.SDGValues.Remove(value);
+            }
+        }
+
+        // Удалим указатель на эту таблицу в указанном ЦУРе
+        var sdg = await _context.SDGs.FindAsync(dto.SDG);
+
+        if (sdg == null)
+        {
+            return NotFound($"SDG {dto.SDG} not found in Database");
+        }
+
+        var splitted = sdg.TableIds.Split(",").Select(int.Parse).ToHashSet();
+        splitted.Remove(dto.SDG);
+        sdg.TableIds = string.Join(",", splitted);
+
+        // Удалим старую таблицу
+        _context.SDGTables.Remove(table);
+
+        // Сперва создадим все значения
+        List<int> valuesIds = [];
+        try
+        {
+            for (var i = 0; i < dto.Headers.Count; i++)
+            {
+                var v = new SDGValue() { Name = dto.Headers[i], Values = dto.Values[i] };
+                var result = await _context.SDGValues.AddAsync(v);
+                await _context.SaveChangesAsync();
+                valuesIds.Add(result.Entity.Id);
+            }
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+
+        // Создадим новую таблицу
+        SDGTable resultEntity;
+        try
+        {
+            var t = new SDGTable() { Name = dto.Name, ValuesIds = string.Join(",", valuesIds), SDG = dto.SDG };
+            var result = await _context.SDGTables.AddAsync(t);
+            await _context.SaveChangesAsync();
+            resultEntity = result.Entity;
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+
+        // Добавим указатель на эту таблицу в указанный ЦУР
+        if (string.IsNullOrEmpty(sdg.TableIds))
+        {
+            sdg.TableIds = resultEntity.Id.ToString();
+        }
+        else
+        {
+            splitted.Add(resultEntity.Id);
+            sdg.TableIds = string.Join(",", splitted);
+            await _context.SaveChangesAsync();
+        }
+
+        await _context.SaveChangesAsync();
         return Ok(resultEntity);
     }
 }
--- Backend/Backend.Web/Data/SDGTable.cs
+++ Backend/Backend.Web/Data/SDGTable.cs
@@ -5,4 +5,5 @@ public class SDGTable
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public string ValuesIds { get; set; } = string.Empty;
+    public int SDG { get; set; }
 }
